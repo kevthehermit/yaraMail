0d90881f152aea62f15fd4adc2aaf92490e42bf9
Kevin Breen <kevthehermit@gmail.com>
2013-04-21 21:01:02 +0100

    Added Reporting and POP Classes
    Added reporting and POP Classes

--- README.md
+++ README.md
@@ -4,39 +4,44 @@ yaraMail
 Yara Scanner For IMAP Feeds and saved Streams
 
 ###What it does:
-- reads an smtp formated email file or connects to imap server
+- reads an smtp formated email file or connects to IMAP / POP server
 - reads emails and extracts attatchments. writes them to your os tmp dir
-- Scans attatchemtns with chosen yara rule file.
 - If emails contains a zip file it extracts all the files and scans them
+- Scans attatchemtns with chosen yara rule file.
+- Writes the results to a Report File
 - deletes the tmp dir created.
 
-###Usage:
+###Usage
+- IMAP Feed
+python yaraMail.py -e -o sampleReport.txt -i -u me@you.com -p password -f inbox sample.yar imap.gmail.com
+- POP Feed
+python yaraMail.py -e -o sampleReport.txt -w -u you@me.com -p password sample.yar pop3.live.com
+- From File
+python yaraMail.py -e -o sampleReport.txt sample.yar SampleMail.txt
 
-python yaraMail.py
-Usage: yaraMail.py [options] smtpFile | imap Folder Name
-Yara Mail Scanner, use it to Scan Email Attatchments
+###Reports
 
-Options:
---version
-		show program's version number and exit
--h, --help
-		show this help message and exit
--r FILE, --Rules=FILE
-		Yara Rule File
--e, --extract
-		Extract Files if Zip Archive detected Deafult is No
-IMAP OPTIONS:
--u USER, --user=USER
-		IMAP UserName
--p PASSWD, --passwd=PASSWD
-		IMAP Password
--s SERVER, --server=SERVER
-		IMAP ServerName
+Here is an example of the report print out
 
-###What it will do:
 
-- Accept POP Accounts.
-- Nicer Output to screen.
-- Write a report file. 
+From: Kevin Breen <email@email.com>  
+Subject: Subject Line  
+Att Name: Name of attatch.ext  
+Matched Rules:  
+Rule_Name1  
+Rule_Name2  
+
+
+
+###Misc
+The Attachement extract also extracts any Body to the EMail in either text/plain or text/HTML format  
+-The text body of the email is typically named as part-001.ksh (this is what python mime guesses the ext as)  
+-The HTML Body of the text is typically named as part-002.html  
+
+###ToDo
+
+-Add verbose output
+
+
 
 
--- yaraMail.py
+++ yaraMail.py
@@ -6,13 +6,12 @@ Python script to YaraScan Email Attatchments
 '''
 __description__ = 'Yara Mail Scanner, use it to Scan Email Attatchments'
 __author__ = 'Kevin Breen'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __date__ = '2013/04/20'
 
 
 import os
 import sys
-import time
 import tempfile
 import shutil
 import email.utils
@@ -22,34 +21,53 @@ try:
 	import yara
 except:
 	print "Failed to Import Yara"
+	sys.exit()
 
 def main():
-	parser = OptionParser(usage='usage: %prog [options] smtpFile | imap Folder Name\n' + __description__, version='%prog ' + __version__)
-	parser.add_option("-r", "--Rules", dest="yaraRule",
-                  help="Yara Rule File", metavar="FILE")
+	parser = OptionParser(usage='usage: %prog [options] rulefile smtpFile|server Address\n' + __description__, version='%prog ' + __version__)
 	parser.add_option("-e", "--extract", action='store_true', default=False, help="Extract Files if Zip Archive detected Deafult is No")
-	group = OptionGroup(parser, "IMAP OPTIONS")
-	group.add_option("-u", "--user", type="string", help="IMAP UserName")
-	group.add_option("-p", "--passwd", type="string", help="IMAP Password")				  
-	group.add_option("-s", "--server", type="string", help="IMAP ServerName")
-
+	parser.add_option("-o", "--output", dest="report", help="Write Results To File", metavar="FILE")
+	parser.add_option("-i", "--imap", action='store_true', default=False, help="Use IMAP Server")
+	parser.add_option("-w", "--pop", action='store_true', default=False, help="Use POP Server")
+	group = OptionGroup(parser, "Web Server Options")
+	group.add_option("-u", "--user", type="string", help="UserName")
+	group.add_option("-p", "--passwd", type="string", help="Password")				  
+	group.add_option("-f", "--folder", type="string", help="FolderName")
 	parser.add_option_group(group)
 	(options, args) = parser.parse_args()
 	global tmpDir
 	tmpDir = tempfile.mkdtemp()
-	global extract
-	extract = options.extract
-	if len(args) != 1:
+	if len(args) != 2:
 		parser.print_help()
-	elif options.user:
-		imapScan(args[0], options.user, options.passwd, options.server, options.yaraRule)
+	elif options.imap == True:
+		emails = imapScan().parse(args[1], options.user, options.passwd, options.folder)
+		for message in emails:
+			attatch = attExtract().parse(message, options.extract)
+			print "Scanning Attatchments"
+			for att in attatch:
+				results = yaraScan().scanner(att["msg"], args[0])
+				if options.report and results:
+					reportMain(options.report, att, results)
+
+	elif options.pop == True:
+		emails = popScan().parse(args[1], options.user, options.passwd)
+		for message in emails:
+			attatch = attExtract().parse(message, options.extract)
+			for att in attatch:
+				results = yaraScan().scanner(att["msg"], args[0])
+				if options.report and results:
+					reportMain(options.report, att, results)
 	else:
-		smtpScan(args[0], options.yaraRule)
+		attatch = attExtract().parse(args[1], options.extract)
+		print "Scanning Attatchments"
+		for att in attatch:
+			results = yaraScan().scanner(att["msg"], args[0])
+			if options.report and results:
+				reportMain(options.report, att, results)
 	shutil.rmtree(tmpDir)
 
-
 class attExtract:
-	def __init__(self, emailFile, yaraRule):
+	def parse(self, emailFile, extract):
 		x = open(emailFile)
 		msg = email.message_from_file(x)
 		x.close()
@@ -58,12 +76,11 @@ class attExtract:
 		fromAdd = msg['from']
 		# taken from the python docs tut
 		counter = 1
-		print extract
+		attatch = []
 		for part in msg.walk():
 			if part.get_content_maintype() == 'multipart':
 				continue
-			filename = part.get_filename()
-			
+			filename = part.get_filename()			
 			if not filename:
 				ext = mimetypes.guess_extension(part.get_content_type())
 				if not ext:
@@ -74,6 +91,7 @@ class attExtract:
 			fp.write(part.get_payload(decode=True))
 			fp.close()
 			ext = filename.split(".")[-1]
+			# if declared extract all the files from the zip and write to tmp
 			if ext == 'zip' and extract == True:
 				from zipfile import ZipFile
 				with ZipFile(os.path.join(tmpDir, filename)) as compressed:
@@ -85,58 +103,77 @@ class attExtract:
 						dest = file(os.path.join(tmpDir, filename), "wb")
 						with source, dest:
 							shutil.copyfileobj(source, dest)
-							print "Scanning %s" % filename
-						yaraScan(filename, yaraRule)
+						attatch.append ({"msg" : (os.path.join(tmpDir, filename)), "subject" : subjectLine, "from" : fromAdd})
 			else:
-				print "Scanning %s" % filename
-				yaraScan(os.path.join(tmpDir, filename), yaraRule)				
-		
-		
-
-		
-class smtpScan:
-	def __init__(self, emailFile, yaraRule):
-		attExtract(emailFile, yaraRule)
-
-
-		
+				attatch.append ({"msg" : (os.path.join(tmpDir, filename)), "subject" : subjectLine, "from" : fromAdd})				
+		return attatch	
+	
 class imapScan:
-	def __init__(self, inbox, user, pwd, server, yaraRule):
+	def parse(self, server, user, pwd, folder):
 		import imaplib
 		print tmpDir		
 		m = imaplib.IMAP4_SSL(server)
 		m.login(user,pwd)
-		m.select(inbox)
+		m.select(folder)
 		resp, items = m.search(None, "ALL") # IMAP Filter Rules here
 		items = items[0].split()
 		count = len(items)
 		counter = 0
+		emails = []
+		print "Fetching %s Emails" % count
 		for emailid in items:
-			emailFile = os.path.join(tmpDir, inbox+emailid + ".txt")
+			emailFile = os.path.join(tmpDir, folder+emailid + ".txt")
 			counter +=1
 			resp, data = m.fetch(emailid, "(RFC822)")
 			email_body = data[0][1]
 			msgFile = open(emailFile, "w")
 			msgFile.write(email_body)
 			msgFile.close()
-			print "Processing %s" % emailFile
-			attExtract(emailFile, yaraRule)
-	
+			emails.append(emailFile)
+		return emails
+
+class popScan:
+	def parse(self, server, user, pwd):
+		import poplib
+		m = poplib.POP3_SSL(server)
+		m.user(user)
+		m.pass_(pwd)
+		emailCount, total_bytes = m.stat()
+		counter = 0
+		emails = []
+		print "Fetching %s Emails" % emailCount
+		for email in range(emailCount):
+			counter +=1
+			emailFile = os.path.join(tmpDir, server+str(counter) + ".txt")
+			msgFile = open(emailFile, "w")
+			for msg in m.retr(email+1)[1]:				
+				msgFile.write(msg)
+				msgFile.write("\n")
+			msgFile.close()
+			emails.append(emailFile)
+		return emails
+
+			
 class yaraScan:
-	def __init__(self, scanfile, yaraRule):
+	def scanner(self, scanfile, yaraRule):
 		yaraRules = yara.compile(yaraRule)
 		matches = []
 		if os.path.getsize(scanfile) > 0:
 			for match in yaraRules.match(scanfile):
 				matches.append({"name" : match.rule, "meta" : match.meta})
-		for m in matches:
-			yaraRule = m["name"]
-			try:
-				yaraDesc = m["meta"]["maltype"]
-			except:
-				yaraDesc = None
-		print matches
+		return matches
 		
-		
+class reportMain:
+	def __init__(self, report, att, results):
+		with open(report, "a") as f:
+			f.write("----------\n")
+			f.write("From: %s\n" % att["from"])
+			f.write("Subject: %s\n" % att["subject"])
+			f.write("Att Name: %s\n" % att["msg"])
+			f.write("Matched Rules: \n")
+			for m in results:
+				f.write(m["name"] + "\n")
+			f.write("----------\n")
+			
 if __name__ == "__main__":
 	main()
